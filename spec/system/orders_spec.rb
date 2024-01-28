1a3b62b39eec21c21b81cd1d75ff7eed55ead203
村上学 <>
2024-01-28 10:40:17 +0900

    ラジオボタンを追加する

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe '注文フォーム', type: :system do
     fill_in 'お届け先住所', with: delivery_address
     select '銀行振込', from: '支払い方法'
     fill_in 'その他・ご要望', with: other_comment
+    choose '配信を希望する'
 
     click_on '確認画面へ'
 
@@ -36,6 +37,7 @@ RSpec.describe '注文フォーム', type: :system do
     expect(order.delivery_address).to eq delivery_address
     expect(order.payment_method_id).to eq 2
     expect(order.other_comment).to eq other_comment
+    expect(order.direct_mail_enabled).to eq true
   end
 
   context '入力情報に不備がある場合' do
@@ -48,12 +50,14 @@ RSpec.describe '注文フォーム', type: :system do
       fill_in 'お届け先住所', with: delivery_address
       select '銀行振込', from: '支払い方法'
       fill_in 'その他・ご要望', with: other_comment
+      choose '配信を希望する'
 
       click_on '確認画面へ'
 
       expect(current_path).to eq confirm_orders_path
       expect(page).to have_content '電話番号は11文字以内で入力してください'
     end
+  end
 
     context '確認画面で戻るボタンを押した場合' do
       it '商品を注文できること' do
@@ -65,6 +69,7 @@ RSpec.describe '注文フォーム', type: :system do
         fill_in 'お届け先住所', with: delivery_address
         select '銀行振込', from: '支払い方法'
         fill_in 'その他・ご要望', with: other_comment
+        choose '配信を希望する'
 
         click_on '確認画面へ'
 
@@ -80,11 +85,15 @@ RSpec.describe '注文フォーム', type: :system do
         expect(page).to have_field 'お届け先住所', with: delivery_address
         expect(page).to have_select '支払い方法', selected: '銀行振込'
         expect(page).to have_field 'その他・ご要望', with: other_comment
+        expect(page).to have_checked_field '配信を希望する'
 
         click_on '確認画面へ'
+        expect(current_path).to eq confirm_orders_path
+
         click_on 'OK'
 
         expect(current_path).to eq complete_orders_path
+        expect(page).to have_content "#{name}様"
 
         order = Order.last
         expect(order.name).to eq name
@@ -93,7 +102,7 @@ RSpec.describe '注文フォーム', type: :system do
         expect(order.delivery_address).to eq delivery_address
         expect(order.payment_method_id).to eq 2
         expect(order.other_comment).to eq other_comment
+        expect(order.direct_mail_enabled).to eq true
       end
     end
-  end
 end
